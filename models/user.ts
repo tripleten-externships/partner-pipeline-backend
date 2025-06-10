e7ef2554732473356a1095094dce13c4e2dec295
Johanna Schnell <johannaschnell@gmail.com>
2025-06-10 11:20:10 -0700

    feat: user-log-table

--- models/user.ts
+++ models/user.ts
@@ -1,7 +1,15 @@
 import { list } from "@keystone-6/core";
 import type { ListConfig } from "@keystone-6/core";
 import type { Lists } from ".keystone/types";
-import { checkbox, text, password, timestamp, select, relationship, json } from "@keystone-6/core/fields";
+import {
+  checkbox,
+  text,
+  password,
+  timestamp,
+  select,
+  relationship,
+  json,
+} from "@keystone-6/core/fields";
 import { UserRoleValues } from "../utils/values";
 
 export const User: ListConfig<Lists.User.TypeInfo<any>, any> = list({
@@ -39,13 +47,13 @@ export const User: ListConfig<Lists.User.TypeInfo<any>, any> = list({
     role: select({
       options: UserRoleValues.map((value) => ({ label: value, value })),
       defaultValue: "Student",
-    //  validation: { isRequired: true },
+      //  validation: { isRequired: true },
     }),
     isAdmin: checkbox({ defaultValue: true }),
     createdAt: timestamp({
       defaultValue: { kind: "now" },
     }),
-   // project: text({ validation: { isRequired: true } }),
+    // project: text({ validation: { isRequired: true } }),
     isActive: checkbox({ defaultValue: false }),
     lastLoginDate: timestamp({
       defaultValue: { kind: "now" },
