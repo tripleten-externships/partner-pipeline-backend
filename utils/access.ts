5906cf3dca1c6f3620dffdfdb8b2315fe1cdaea6
Brittany Tylutke <tylub001@gmail.com>
2025-10-13 11:46:11 -0400

    feat(reminder): implement reminder logic and unresponsive status update

--- utils/access.ts
+++ utils/access.ts
@@ -9,24 +9,24 @@ type Session = {
 export const isSignedIn = ({ session }: { session?: Session }) => !!session;
 
 export const permissions = {
-  isStudent: ({ session }: { session?: Session }) => session?.data.role === "Student",
+  // isStudent: ({ session }: { session?: Session }) => session?.data.role === "Student",
 
-  isProjectMentor: ({ session }: { session?: Session }) => session?.data.role === "Project Mentor",
+  // isProjectMentor: ({ session }: { session?: Session }) => session?.data.role === "Project Mentor",
 
-  isLeadMentor: ({ session }: { session?: Session }) => session?.data.role === "Lead Mentor",
+  // isLeadMentor: ({ session }: { session?: Session }) => session?.data.role === "Lead Mentor",
 
-  isExternalPartner: ({ session }: { session?: Session }) =>
-    session?.data.role === "External Partner",
+  //isExternalPartner: ({ session }: { session?: Session }) =>
+  // session?.data.role === "External Partner",
 
-  isAdminLike: ({ session }: { session?: Session }) =>
-    ["Admin", "Lead Mentor", "Project Mentor"].includes(session?.data.role ?? ""),
-  isProjectMember: ({ session }: { session?: Session }) => session?.data.project === "",
+  //isAdminLike: ({ session }: { session?: Session }) =>
+  //["Admin", "Lead Mentor", "Project Mentor"].includes(session?.data.role ?? ""),
+  //isProjectMember: ({ session }: { session?: Session }) => session?.data.project === "",
 
   // UNCOMMENT FOR TESTING PURPOSES
 
-  // isStudent: () => true,
-  // isProjectMentor: () => true,
-  // isLeadMentor: () => true,
-  // isExternalPartner: () => true,
-  // isAdminLike: () => true,
+  isStudent: () => true,
+  isProjectMentor: () => true,
+  isLeadMentor: () => true,
+  isExternalPartner: () => true,
+  isAdminLike: () => true,
 };
