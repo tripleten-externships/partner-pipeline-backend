c88456d21fafc51f5e19e2be2b6fb9b06c69408c
Hunter Melrose <huntermelrose831@gmail.com>
2025-10-17 16:22:07 -0700

    feat: create csv import endpoint for waitlist page

--- migrations/20250901235145_test1/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `before` on the `invitationtokenlog` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `invitationtokenlog` DROP COLUMN `before`;
