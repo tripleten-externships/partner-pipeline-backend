49b52fcba4c10294085196d3dbb04696dadbfe8d
paxhuckstep <paxhuckstep@gmail.com>
2025-10-22 20:45:33 -0600

    chore(milestone): removed unneeded comments

--- routes/milestoneDataRoutes.ts
+++ routes/milestoneDataRoutes.ts
@@ -18,7 +18,7 @@ export function createMilestoneRouter(commonContext: Context<BaseKeystoneTypeInf
     // if context exists, run controller function
     await getProjectMilestones(req, res, context);
   });
-  // NEW PAX CODE -- TOP
+
   router.post("/api/projects/:projectId/milestones", async (req, res) => {
     const context = await commonContext.withRequest(req, res);
     if (!context) return res.status(500).send("Failed to get context");
@@ -26,7 +26,6 @@ export function createMilestoneRouter(commonContext: Context<BaseKeystoneTypeInf
     // if context exists, run controller function
     await createProjectMilestone(req, res, context); // NOT REAL, NOT MADE, NAMED BY PAX -- CAN/probably will BE CHANGED
   });
-  // NEW PAX CODE -- BOTTOM
 
   return router;
 }
